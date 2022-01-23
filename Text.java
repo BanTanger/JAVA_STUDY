0ece0ffc9eea37198f6a7fb6afd6e01a11f44640
BanTanger <88583317+BanTanger@users.noreply.github.com>
2022-01-23 22:02:12 +0800

    Add files via upload

---
+++ Text.java
@@ -0,0 +1,12 @@
+// 从零开始 第一个程序
+// 1 + 1 的结果，从计算机命令行开始
+
+public class Test {
+	
+	public static void main (String[] args) {
+		
+		int res = 1 + 1;
+		//显示
+		System.out.println("结果=”+ res);
+	}
+}
